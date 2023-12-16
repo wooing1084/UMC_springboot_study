a740537e56029afe71772144f3687bcd936e723e
SungHoon Jung <32007781+wooing1084@users.noreply.github.com>
2023-12-16 14:27:19 +0900

    ADD: 진행중인 미션 목록

--- spring/src/main/java/umc/spring/service/MissionApplyService/MissionApplyQueryServiceImpl.java
+++ spring/src/main/java/umc/spring/service/MissionApplyService/MissionApplyQueryServiceImpl.java
@@ -1,11 +1,16 @@
 package umc.spring.service.MissionApplyService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import umc.spring.domain.Mission;
+import umc.spring.domain.Store;
+import umc.spring.domain.User;
 import umc.spring.domain.mapping.MissionApply;
 import umc.spring.repository.MissionApplyRepository;
 import umc.spring.repository.MissionRepository;
+import umc.spring.repository.UserRepository;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,9 +19,18 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class MissionApplyQueryServiceImpl implements MissionApplyQueryService {
     private final MissionApplyRepository missionApplyRepository;
+    private final UserRepository userRepository;
 
     @Override
     public Optional<List<MissionApply>> findByMissionIdAndUserId(Long userId, Long missionId) {
         return missionApplyRepository.findByMissionIdAndUserId(missionId, userId);
+    }
+
+    @Override
+    public Page<MissionApply> getMissionApplyList(Long userId, Integer page){
+        User user = userRepository.findById(userId).get();
+
+        Page<MissionApply> missionPage = missionApplyRepository.findAllByUser(user, PageRequest.of(page, 10));
+        return missionPage;
     }
 }
