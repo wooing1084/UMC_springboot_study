1ace733f8ec90cd22661f6cd562d5ea753c2e59c
SungHoon Jung <32007781+wooing1084@users.noreply.github.com>
2023-12-16 11:19:43 +0900

    ADD:특정 가게 미션 목록

--- spring/src/main/java/umc/spring/service/MissionService/MissionQueryServiceImpl.java
+++ spring/src/main/java/umc/spring/service/MissionService/MissionQueryServiceImpl.java
@@ -1,12 +1,17 @@
 package umc.spring.service.MissionService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import umc.spring.apiPayload.code.status.ErrorStatus;
 import umc.spring.apiPayload.exception.handler.TempHandler;
 import umc.spring.domain.Mission;
+import umc.spring.domain.Review;
+import umc.spring.domain.Store;
 import umc.spring.domain.User;
 import umc.spring.repository.MissionRepository;
+import umc.spring.repository.StoreRepository;
 import umc.spring.repository.UserRepository;
 
 import java.util.Optional;
@@ -15,9 +20,18 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class MissionQueryServiceImpl implements MissionQueryService {
     private final MissionRepository missionRepository;
+    private final StoreRepository storeRepository;
 
     @Override
     public Optional<Mission> findMission(Long id) {
         return missionRepository.findById(id);
+    }
+
+    @Override
+    public Page<Mission> getMissionList(Long storeId, Integer page){
+        Store store = storeRepository.findById(storeId).get();
+
+        Page<Mission> missionPage = missionRepository.findAllByStore(store, PageRequest.of(page, 10));
+        return missionPage;
     }
 }
--- spring/src/main/java/umc/spring/web/controller/StoreRestController.java
+++ spring/src/main/java/umc/spring/web/controller/StoreRestController.java
@@ -10,37 +10,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import umc.spring.apiPayload.ApiResponse;
-import umc.spring.converter.ReviewConverter;
-import umc.spring.converter.UserConverter;
-import umc.spring.domain.User;
-import umc.spring.service.UserService.UserCommandService;
-import umc.spring.service.UserService.UserQueryService;
+import umc.spring.converter.MissionConverter;
+import umc.spring.service.MissionService.MissionQueryService;
 import umc.spring.validation.annotation.CheckPage;
-import umc.spring.validation.annotation.ExistUsers;
-import umc.spring.web.dto.ReviewResponseDTO;
-import umc.spring.web.dto.UserRequestDTO;
-import umc.spring.web.dto.UserRequestDTO.JoinDTO;
-import umc.spring.web.dto.UserResponseDTO;
-
-import javax.validation.Valid;
-import javax.validation.constraints.Min;
+import umc.spring.validation.annotation.ExistStores;
+import umc.spring.web.dto.MissionResponseDTO;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/store")
 @Validated
-public class UserRestController {
-    private final UserCommandService userCommandService;
-    private final UserQueryService userQueryService;
+public class StoreRestController {
+    private final MissionQueryService missionQueryService;
 
-    @PostMapping("/")
-    public ApiResponse<UserResponseDTO.JoinResultDTO> join(@RequestBody @Valid UserRequestDTO.JoinDTO request){
-        User user = userCommandService.joinUser(request);
-        return ApiResponse.onSuccess(UserConverter.toJoinResultDTO(user));
-    }
-
-    @GetMapping("/{userId}/reviews")
-    @Operation(summary = "유저의 리뷰 목록 조회", description = "유저의 리뷰 목록을 조회합니다.")
+    @GetMapping("/{storeId}/missions")
+    @Operation(summary = "가게의 미션 목록 조회", description = "가게의 미션 목록을 조회합니다.")
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공"),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access 토큰을 주세요!",content = @Content(schema = @Schema(implementation = io.swagger.annotations.ApiResponse.class))),
@@ -48,11 +32,11 @@ public class UserRestController {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH006", description = "acess 토큰 모양이 이상함",content = @Content(schema = @Schema(implementation = io.swagger.annotations.ApiResponse.class))),
     })
     @Parameters({
-            @Parameter(name = "userId", description = "유저의 아이디, path variable 입니다!"),
+            @Parameter(name = "storeId", description = "가게의 아이디, path variable 입니다!"),
             @Parameter(name = "page", description = "페이지 번호, 1부터 입니다!")
     })
-    public ApiResponse<ReviewResponseDTO.ReviewPreViewListDTO> getReviewList(@ExistUsers @PathVariable(name = "userId") Long userId, @CheckPage @RequestParam(name = "page") Integer page){
+    public ApiResponse<MissionResponseDTO.MissionPreViewListDTO> getReviewList(@ExistStores @PathVariable(name = "storeId") Long storeId, @CheckPage @RequestParam(name = "page") Integer page){
         Integer pageIndex = page - 1;
-        return ApiResponse.onSuccess(ReviewConverter.reviewPreViewListDTO(userQueryService.getReviewList(userId, pageIndex)));
+        return ApiResponse.onSuccess(MissionConverter.missionPreViewListDTO(missionQueryService.getMissionList(storeId, pageIndex)));
     }
 }
